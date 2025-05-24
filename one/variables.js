bc83bc1398b42bcf94a93967b1be93ca5d18ce26
AbhimanyuAdhikari <login.abhimanyuadhikari@gmail.com>
2025-05-24 14:13:31 +0545

    variables edit

---
+++ one/variables.js
@@ -0,0 +1,9 @@
+const uId = 123
+let name = "Abhimanyu"
+var age = 20
+subject = "Maths"
+// 4 ways of defining variables in js
+/*
+    const for constants
+    let for variables
+    and other two are also for variabes but not used and must not be used */
