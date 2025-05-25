e2686f9e69da9f4cb4beedd0240de3e35aebe4f1
AbhimanyuAdhikari <login.abhimanyuadhikari@gmail.com>
2025-05-25 09:18:20 +0545

    conversion basics

---
+++ one/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score = "33"; //string
+let numberScore = Number(score); // number note that N will be capital
+
+console.log(typeof score); // string
+console.log(typeof (numberScore)); // number
+
+// '33abc' => NaN // returns not a number but type will be number
+// true => 1
+// null => 0
+// undefined => NaN
