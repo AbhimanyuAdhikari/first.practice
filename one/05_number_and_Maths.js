265185305cb9c45408e255c31b2e55955b50144c
AbhimanyuAdhikari <login.abhimanyuadhikari@gmail.com>
2025-06-05 00:24:14 +0545

    maths learn

---
+++ one/05_number_and_Maths.js
@@ -0,0 +1,6 @@
+console.log(Math.random());// prints num between 0 and 1
+
+console.log(Math.ceil(5.9)); //ceiling value
+console.log(Math.floor(7.1));
+console.log(Math.floor(Math.random() * 10    ));
+
