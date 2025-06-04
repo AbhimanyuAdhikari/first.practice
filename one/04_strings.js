a11062e01614dc6d503990997fd89bc8a26c914f
AbhimanyuAdhikari <login.abhimanyuadhikari@gmail.com>
2025-06-04 13:36:41 +0545

    string

---
+++ one/04_strings.js
@@ -0,0 +1,26 @@
+const name ="abhimanyu-adhikari"
+const age = 25
+
+console.log(name+age);
+
+console.log(`Hello my name is ${name} and age is ${age}`);
+
+console.log(name[0]); //prints A
+
+console.log(name[3]); //prints i
+
+console.log(name.length);
+
+console.log(name.toUpperCase());
+
+console.log(name.charAt(6));
+
+console.log(name.indexOf('n'));//prints 6
+
+console.log(name.indexOf('a')); // prints the index of a that comes first i.e 0
+
+console.log(name.replace('-','*'))
+
+console.log(name.trim());// trims the whitespaces in front and end of the string
+
+console.log(name.includes('hik')); //checks if hik is included in the string
